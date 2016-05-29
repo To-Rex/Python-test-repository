a92ff0ac04205c84f59afc5c9236a53c671b1380
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2016-05-30 01:07:21 +0500

    commit

--- updateDay.py
+++ updateDay.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 for i in range(0,randint(1,3)):
-    d = '2016-5-29'
+    d = '2016-5-30'
     with open('file.txt', 'a') as file:
         file.write(d + "\n")
     os.system('git add .')
