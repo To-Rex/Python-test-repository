021b1091877d57c4c32b73b63f2a96961d33a55c
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2019-03-01 00:00:00 +0500

    commit

--- updateWeekDay.py
+++ updateWeekDay.py
@@ -3,7 +3,7 @@ from random import randint
 
 for i in range(0,13):
     for j in range(0,randint(5,11)):
-        d = '2018-3-' + str(i+1) + ' ' + str(j) + ':00:00'
+        d = '2019-3-' + str(i+1) + ' ' + str(j) + ':00:00'
         with open('file.txt', 'a') as file:
             file.write(d + "\n")
         os.system('git add .')
