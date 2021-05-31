7d7b3e213fc685de45530d73e221a56fdcfa1535
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2021-06-01 00:00:00 +0500

    commit

--- updateWeekDay.py
+++ updateWeekDay.py
@@ -1,10 +1,10 @@
 import os
 from random import randint
 
-date = '2021-6-20'
+date = '2021-6-23'
 for i in range(0,7):
     for j in range(0,randint(5,11)):
-        d = date + ' ' + str(i) + ' hours ago'
+        d = '2021-6-' + str(i+1) + ' ' + str(j) + ':00:00'
         with open('file.txt', 'a') as file:
             file.write(d + "\n")
         os.system('git add .')
