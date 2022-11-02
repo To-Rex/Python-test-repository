be8e469547afc5e46fefcf4a28365c00281ab5fa
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-03 03:04:52 +0500

    commit

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 
 for i in range(1,365):
-    for j in range(0,randint(5,8)):
+    for j in range(0,randint(5,11)):
         d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
             file.write(d + "\n")
