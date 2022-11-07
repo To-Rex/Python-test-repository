10e5db8bb144d0a8a5264adb6af7d121ae110e9c
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-07 22:07:58 +0500

    commit

--- application.py
+++ application.py
@@ -1,12 +1,20 @@
 import os
 from random import randint
 
-for i in range(1,365):
-    for j in range(0,randint(5,11)):
-        d = str(i) + ' days ago'
-        with open('file.txt', 'a') as file:
-            file.write(d + "\n")
-        os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
+# for i in range(1,365):
+#     for j in range(0,randint(5,11)):
+#         d = str(i) + ' days ago'
+#         with open('file.txt', 'a') as file:
+#             file.write(d + "\n")
+#         os.system('git add .')
+#         os.system('git commit --date="' + d + '" -m "commit"')
+
+# current day commit
+for i in range(0,randint(5,11)):
+    d = '0 days ago'
+    with open('file.txt', 'a') as file:
+        file.write(d + "\n")
+    os.system('git add .')
+    os.system('git commit --date="' + d + '" -m "commit"')
 
 os.system('git push origin master')
