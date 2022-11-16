eb7af9b81e8eff95de715ceb3ea76961a063ec5b
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-16 10:44:08 +0500

    commit

--- updateDay.py
+++ updateDay.py
@@ -1,7 +1,7 @@
 import os
 from random import randint
 
-for i in range(0,randint(1,100)):
+for i in range(0,randint(1,50)):
     d = '2022-11-16'
     with open('file.txt', 'a') as file:
         file.write(d + "\n")
