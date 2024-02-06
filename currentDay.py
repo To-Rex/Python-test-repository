69546908de91e6d618ba3237060988a134d7a936
to-rex <torex.amaki@gmail.com>
2024-02-06 21:37:49 +0500

    commit

--- currentDay.py
+++ currentDay.py
@@ -1,11 +1,25 @@
 import os
 from random import randint
 
+# for i in range(0,randint(5,161)):
+#     d = '0 days ago'
+#     with open('file.txt', 'a') as file:
+#         file.write(d + "\n")
+#     os.system('git add .')
+#     os.system('git commit --date="' + d + '" -m "commit"')
+# os.system('git push origin master')
+
+# cd existing_repo
+# git remote add origin https://gitlab.com/To-Rex/python-test.git
+# git branch -M main
+# git push -uf origin main
+#pushing gitlab
+
 for i in range(0,randint(5,161)):
     d = '0 days ago'
     with open('file.txt', 'a') as file:
         file.write(d + "\n")
     os.system('git add .')
     os.system('git commit --date="' + d + '" -m "commit"')
-
 os.system('git push origin master')
+os.system('git push -uf origin master')
